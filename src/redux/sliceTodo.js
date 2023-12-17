4f6af9762f41c7a58acaed171260f76ff2d82326
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-12-17 12:01:09 +0200

    123

---
+++ src/redux/sliceTodo.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  todos: [],
+};
+export const sliceTodo = createSlice({
+  name: 'todo',
+  initialState,
+
+  reducers: {
+    addTodo(state, action) {
+      state.todos = [...state.todos, action.payload];
+    },
+    deleteTodo(state, action) {
+      state.todos = state.todos.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = sliceTodo.actions;
+
+export const todoReducer = sliceTodo.reducer;
+
+export const selectTodos = state => state.todos;
